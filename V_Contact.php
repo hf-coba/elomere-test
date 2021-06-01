157e505e85fafce813402e2f8bbd6b6ae7048eaf
hf-coba1 <idasofiyah27@gmail.com>
2021-06-01 15:57:10 +0700

    Test 2

--- V_Contact.php
+++ V_Contact.php
@@ -5,6 +5,52 @@
 			<h2>Contact Me</h2>
 		</div>
 	</div>
-	<p>Facebook : <a href="https://www.facebook.com/malasngodingpage">https://www.facebook.com/malasngodingpage</a>.</p>
-	
+	<p>git init<br>
+		git remote add origin https://github.com/hf-coba/????.git<br>
+		git branch -M main<br>
+
+		> git add ./nama file<br>
+		> git pull ( sync last commit ) -- ambil data dr remote, merge ke lokal<br>
+		> git rm (nama file/folder)<br>
+
+		git commit -m "(pesan)"<br>
+		git push -u origin main</p>
+
+
+
+	<div class="row">
+		<div class="col">
+				 <div class="clock">
+				<div class="display">
+				</div>
+				</div>
+		</div>
+	</div>
 </div>
+<script>
+      setInterval(function(){
+        const clock = document.querySelector(".display");
+        let time = new Date();
+        let sec = time.getSeconds();
+        let min = time.getMinutes();
+        let hr = time.getHours();
+        let day = 'AM';
+        if(hr > 12){
+          day = 'PM';
+          hr = hr - 12;
+        }
+        if(hr == 0){
+          hr = 12;
+        }
+        if(sec < 10){
+          sec = '0' + sec;
+        }
+        if(min < 10){
+          min = '0' + min;
+        }
+        if(hr < 10){
+          hr = '0' + hr;
+        }
+        clock.textContent = hr + ':' + min + ':' + sec + " " + day;
+      });
+    </script>
--- index.php
+++ index.php
@@ -16,9 +16,224 @@
 	<!-- My CSS -->
 	<link rel="stylesheet" type="text/css" href="style.css">
 
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans:wght@700&family=Poppins:wght@400;500;600&display=swap');
+
+
+.wrapperAds{
+  position: absolute;
+  max-width: 400px;
+  top: 50%;
+  left: 50%;
+  width: 100%;
+  padding: 40px 30px;
+  background: #fff;
+  border-radius: 15px;
+  opacity: 0;
+  pointer-events: none;
+  transform: translate(-50%, -50%) scale(1.2);
+  box-shadow: 10px 10px 15px rgba(0,0,0,0.06);
+  transition: opacity 0.2s 0s ease-in-out,
+  transform 0.2s 0s ease-in-out;
+}
+
+.wrapperAds.show{
+  opacity: 1;
+  pointer-events: auto;
+  transform:translate(-50%, -50%) scale(1);
+}
+
+.wrapperAds .wrap-konten,
+.wrap-konten .ikon-seru,
+.ikon-seru .gmbr-seru{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column; /* Flex diatur model kolom */
+}
+
+.wrap-konten .ikon-seru{
+  height: 115px;
+  width: 115px;
+  border-radius: 50%;
+  background: linear-gradient(#9b27ca 0%, #9927cf 0%, #d33639 100%, #f92121 100%);
+}
+
+.ikon-seru .gmbr-seru{
+  height: 80px; 
+  width: 80px;
+  background: #fff;
+  /* border-radius: inherit; atau 50%; /* buat radius kotak border jadi lingkaran */
+}
+
+.ikon-seru .gmbr-seru i{
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  font-size: 100px;
+}
+
+.wrap-konten h2{
+  margin-top: 35px;
+  font-size: 32px;
+}
+
+.wrap-konten p{
+  font-size: 19px;
+  text-align: center;
+  margin-top: 20px;
+}
+
+.btn_wrapperAds{
+  height: 60px;
+  width: 220px;
+  margin-top: 30px;
+  border-radius: 50px;
+  position: relative;
+  
+  /* Beri tanda komentar bila bg dianimasi, agar warna bg yg diambil dari fungsi .btn_wrapper .bg-layer */
+  background: -webkit-linear-gradient(135deg, #9b27ca, #d33639, #9b27ca, #d33639);
+  overflow: hidden; /* ganti hidden ke visible utk lihat elemen aslinya */
+}
+
+/* ==> Set Fungsi ini bila button wrapperAds perlu di ANIMASI */
+/* .btn_wrapperAds .bg-layer{
+  height: 100%;
+  width: 200%;
+  position: absolute;
+  left: -100%;
+  background: -webkit-linear-gradient(135deg, #9b27ca, #d33639, #9b27ca, #d33639);
+  transition: all 0.4s ease;
+} */
+
+.btn_wrapperAds:hover .bg-layer{
+  left: 0;
+}
+
+.wrap-konten button{
+  position: relative;
+  z-index: 1;
+  height: 100%;
+  width: 100%;
+  background: none;
+  font-size: 18px;
+  border: none;
+  outline: none;
+  color: #fff;
+  cursor: pointer;
+}
+
+/* ---------- Wrapper Cookie Browser ------------------- */
+/* Set Model Kotak
+.wrapper{
+  position: absolute;
+  bottom: 30px;
+  right: 30px;
+  max-width: 360px;
+  background: #fff;
+  padding: 10px 10px 10px 10px;
+  border-radius: 15px;
+  box-shadow: 1px 7px 14px -5px rgba(0,0,0,0.15);
+  text-align: center;
+}
+*/
+
+.wrapper{
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+    max-width: 360px;
+  background: #fff;
+  padding: 10px 10px 10px 10px;
+  border-radius: 15px;
+  box-shadow: 1px 7px 14px -5px rgba(0,0,0,0.15);
+  text-align: center;
+}
+
+.wrapper.hide{
+  opacity: 0;
+  pointer-events: none;
+  transform: scale(0.8);
+  transition: all 0.3s ease;
+}
+
+.wrapper img{
+  max-width: 100px;
+}
+
+.content header{
+  font-size: 25px;
+  font-weight: 600;
+  padding:0px;
+  /*   background-color:red; */
+}
+
+.content{
+  /* margin-top: 10px; */
+}
+
+.content p{
+  color: #858585;
+  /* margin: 5px 0 20px 0; */
+}
+.content .buttons{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.buttons button{
+  padding: 10px 20px;
+  border: none;
+  outline: none;
+  color: #fff;
+  font-size: 16px;
+  font-weight: 500;
+  border-radius: 5px;
+  background: #FCBA7F;
+  cursor: pointer;
+  transition: all 0.3s ease;
+}
+.buttons button:hover{
+  transform: scale(0.97);
+}
+.buttons .item{
+  margin: 0 10px;
+}
+.buttons a{
+  color: #FCBA7F;
+}
+
+</style>
 </head>
 <body>
 
+	<div id="detectAds"></div>
+	
+	<div class="wrapperAds">
+		<div class="wrap-konten">
+			<div class="ikon-seru">
+				<span class="gmbr-seru"><i class="fas fa-exclamation"></i></span>
+			</div>
+			<h2>AdBlock Terdeteksi</h2>
+			<p>Website ini menampilkan Ads kepada visitor. Please supporting us by whitelisting our website.</p>
+			<div class="btn_wrapperAds">
+				<div class="bg-layer"></div>
+				<button>Okay, I'll Whitelist</button>
+			</div>
+		</div>
+	</div>
+  
+	<div class="wrapper">
+    <!--  img src="cookie.png" alt="" -->
+		<div class="content">
+			<header>Cookies Consent</header>
+			<p>This website use cookies to ensure you get the best experience on our website.</p>
+			<div class="buttons">
+				<button class="item">I understand</button>
+				<a href="#" class="item">Learn more</a>
+			</div>
+		</div>
+	</div>
 	<?php 
 	// Buat Progress Bar
 	/*echo' <div id="myProgresss">
@@ -33,7 +248,7 @@
 		<p>Menguasai Bahasa : HTML, CSS, PHP, WebSocket, JavaScript ( Ajax, JQuery, VueJS, ReactJS, Express JS ), Python, C, C++, C#, dan ASP</p>
 	</div>
 	
-	<div class="content">
+	<div class="area-utama-konten">
 	
 		<header>
 			
@@ -83,4 +298,42 @@
 	
 	<!-- My Script JS -->
 	<script type="text/javascript" src="cekrip.js" ></script>
+	
+	<script>
+	// ====> Wrapper Ads Blocker
+    const detectAds = document.querySelector("#detectAds");
+    const wrapperAds = document.querySelector(".wrapperAds");
+    const buttonAds = wrapperAds.querySelector("button");
+ 
+    let adClasses = ["ad", "ads", "adsbox", "doubleclick", "ad-placement", "ad-placeholder", "adbadge", "BannerAd"];
+    for(let item of adClasses){
+      detectAds.classList.add(item);
+    }
+    let getProperty = window.getComputedStyle(detectAds).getPropertyValue("display");
+    if(!wrapperAds.classList.contains("show")){
+      getProperty == "none" ? wrapperAds.classList.add("show") : wrapperAds.classList.remove("show");
+    }
+    buttonAds.addEventListener("click", ()=>{
+      wrapperAds.classList.remove("show");
+    });
+	
+	// ====> Wrappper Cookies
+	const cookieBox = document.querySelector(".wrapper"),
+    acceptBtn = cookieBox.querySelector("button");
+ 
+    acceptBtn.onclick = ()=>{
+      //setting cookie for 1 month, after one month it'll be expired automatically
+      document.cookie = "CookieBy=CodingNepal; max-age="+60*60*24*30;
+      if(document.cookie){ //if cookie is set
+        cookieBox.classList.add("hide"); //hide cookie box
+      }else{ //if cookie not set then alert an error
+        alert("Cookie can't be set! Please unblock this site from the cookie setting of your browser.");
+      }
+    }
+    let checkCookie = document.cookie.indexOf("CookieBy=CodingNepal"); //checking our cookie
+    //if cookie is set then hide the cookie box else show it
+    checkCookie != -1 ? cookieBox.classList.add("hide") : cookieBox.classList.remove("hide");
+	
+    </script>
+  
 </html>
